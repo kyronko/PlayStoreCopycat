6a0fb8f30639e7c9c65388b778ebcec41f533ef9
user <kyronko@naver.com>
2019-04-21 12:39:58 +0900

    앱 상세 화면 초안 작성

--- app/src/main/java/com/tjedit/playstorecopycat/AppDetailActivity.java
+++ app/src/main/java/com/tjedit/playstorecopycat/AppDetailActivity.java
@@ -14,5 +14,7 @@ public class AppDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         act = DataBindingUtil.setContentView(this,R.layout.activity_app_detail);
+
+
     }
 }
