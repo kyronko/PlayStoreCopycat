11aaa46dece76d21da717d90c80d1dc040454399
user <kyronko@naver.com>
2019-04-21 12:11:44 +0900

    이벤트 비교용 확인버튼 이벤트 추가.

--- app/src/main/java/com/tjedit/playstorecopycat/MainActivity.java
+++ app/src/main/java/com/tjedit/playstorecopycat/MainActivity.java
@@ -3,6 +3,9 @@ package com.tjedit.playstorecopycat;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.tjedit.playstorecopycat.adapters.AppAdapter;
 import com.tjedit.playstorecopycat.databinding.ActivityMainBinding;
@@ -16,16 +19,25 @@ public class MainActivity extends AppCompatActivity {
     List<App> appList = new ArrayList<>();
     AppAdapter mAppAdapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         act = DataBindingUtil.setContentView(this, R.layout.activity_main);
         act.titleTxt.setText("제목 화면");
 
+
         fillApps();
 
         mAppAdapter = new AppAdapter(MainActivity.this,appList);
         act.appRankListView.setAdapter(mAppAdapter);
+
+      act.okBtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Toast.makeText(MainActivity.this, "확인버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+          }
+      });
     }
 
     void fillApps() {
