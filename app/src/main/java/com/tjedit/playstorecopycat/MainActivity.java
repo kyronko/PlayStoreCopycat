767ce8ed1329be95563e8e11d154ae89b44e4668
user <kyronko@naver.com>
2019-04-21 11:40:57 +0900

    액티비티에서 어댑터를 객체화 (New AppAdapter

--- app/src/main/java/com/tjedit/playstorecopycat/MainActivity.java
+++ app/src/main/java/com/tjedit/playstorecopycat/MainActivity.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tjedit.playstorecopycat.adapters.AppAdapter;
 import com.tjedit.playstorecopycat.databinding.ActivityMainBinding;
 import com.tjedit.playstorecopycat.datas.App;
 
@@ -13,22 +14,26 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding act;
     List<App> appList = new ArrayList<>();
+    AppAdapter mAppAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        act = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
         act.titleTxt.setText("제목 화면");
 
         fillApps();
 
-
+        mAppAdapter = new AppAdapter(MainActivity.this,appList);
+        act.appRankListView.setAdapter(mAppAdapter);
     }
-    void fillApps(){
-        appList.add(new App(1, "아스팔트 8 :에어본","GameLoft", 5,6000,true));
-        appList.add(new App(2, "MineCraft","Mojang", 4,6000,true));
-        appList.add(new App(3, "아스팔트 7  : 하트","GameLoft", 2,6000,false));
-        appList.add(new App(4, "팔라독","FrazeCat", 3,6000,true));
-        appList.add(new App(5, "Planet Vs Zombies","EA swiss sarl", 5,6000,false));
-        appList.add(new App(6, "스왐피","Disney", 1,6000,false));
+
+    void fillApps() {
+        appList.add(new App(1, "아스팔트 8 :에어본", "GameLoft", 5, 6000, true));
+        appList.add(new App(2, "MineCraft", "Mojang", 4, 6000, true));
+        appList.add(new App(3, "아스팔트 7  : 하트", "GameLoft", 2, 6000, false));
+        appList.add(new App(4, "팔라독", "FrazeCat", 3, 6000, true));
+        appList.add(new App(5, "Planet Vs Zombies", "EA swiss sarl", 5, 6000, false));
+        appList.add(new App(6, "스왐피", "Disney", 1, 6000, false));
     }
 }
