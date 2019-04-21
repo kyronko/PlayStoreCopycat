fcb3691f78701dd041f1db609aa4b4e27239fe7a
user <kyronko@naver.com>
2019-04-21 12:30:22 +0900

    앱 상세 액티비티 추가 / 데이터 바인디 마무리

---
+++ app/src/main/java/com/tjedit/playstorecopycat/AppDetailActivity.java
@@ -0,0 +1,18 @@
+package com.tjedit.playstorecopycat;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tjedit.playstorecopycat.databinding.ActivityAppDetailBinding;
+import com.tjedit.playstorecopycat.databinding.ActivityMainBinding;
+
+public class AppDetailActivity extends AppCompatActivity {
+
+     ActivityAppDetailBinding act;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        act = DataBindingUtil.setContentView(this,R.layout.activity_app_detail);
+    }
+}
